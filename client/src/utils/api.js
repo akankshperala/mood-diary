9dac75f8214bd8eb206806fd0213bc0922467393
akankshperala <akankshperala@gmail.com>
2025-08-30 17:51:02 +0530

    commited many times

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://mood-diary-1-3ma9.onrender.com",
-  withCredentials: true,
+  baseURL: "https://mood-diary-3.onrender.com/api",
+  // withCredentials: true,
 });
 
 export default api;
