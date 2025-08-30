b7d63a085099ba69cbc2e8f76c034859f7e62ef0
akankshperala <akankshperala@gmail.com>
2025-08-30 16:29:03 +0530

    commited

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5050/api",
+  baseURL: "https://mood-diary-g3gd.onrender.com",
   withCredentials: true,
 });
 
